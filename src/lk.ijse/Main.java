64ef5ce39ada58fa5a006ee93560f70b8e2c4347
Rukshan123 <66395972+Rukshan123@users.noreply.github.com>
2021-08-07 12:55:14 +0530

    update main file

---
+++ src/lk.ijse/Main.java
@@ -0,0 +1,31 @@
+package lk.pirate.demoFx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/lk//git-hub-quizz/view/dashbord.fxml")));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            //primaryStage.initStyle(StageStyle.TRANSPARENT);
+            //primaryStage.setFullScreen(true);
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
